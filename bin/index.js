10b25baa3de591970f2f6e25cc7995d29f52df11
Qiansimin <qiansimin@shining3d.com>
2017-04-17 14:34:50 +0800

    fix

--- bin/index.js
+++ bin/index.js
@@ -17,6 +17,12 @@ const w = chalk.bold.red;      //红粗
 const promiseItem = Promise.resolve;
 require('shelljs/global');
 
+let mostEnv = [ 'develop', 'daily', 'test' ]; 
+let runEnvCollect = {
+};
+let runModeEnv = null; //执行环境
+
+let commanderType = 'update';   //当前命令的模式
 let rootPath = '';  //根目录
 let updatePackgeName = '';  //需要升级的包名字
 let updateVersion = '';  //将要升级的版本号
@@ -31,7 +37,7 @@ function std (code, str) {
     process.exit(code);
 };
 //处理IO
-function handlerAllFile( c, k, s ) {
+function handlerAllFile( c, k, s, mode ) {
     let allPathAarray = [];
     new Promise((resolve, reject) => {
          fs.readdir( c, (err, files) => {
@@ -58,64 +64,75 @@ function handlerAllFile( c, k, s ) {
             resolve( allPathAarray );
         });
     }).then( dirdata => {
-        var allpipie = [];
-        var itempipe = [];
-        dirdata.map( (o, i) => {
-            var itemPromise = new Promise((resolve, reject) => {
-                handlerPackage( o )
-                    .then(packdata => {
-                        if( packdata !== 'null' ) {
-                            resolve( o );
-                        }else {
-                            resolve( false );
-                        }
-                    })
-            })
-            itempipe.push(itemPromise);
-        });
-        
-        Promise.all(itempipe)
-            .then(data => {
-               data.filter( o => o !== false).map((_ , i) => {
-                    allFileArray[i] = [ i, _, '', updateVersion ];
-                    tablePrinter.push( [i , _, '', updateVersion ] );
-                    allpipie.push(handlerPackage( _ ));
-               });
-               return data;
-            })
-            .then(itemData => {
-                return Promise.all(allpipie)
-                    .then(packdataArray => {
-                        packdataArray.map((o, i) => {
-                            allFileArray[i][2] = o;
-                            tablePrinter[i][2] = o;
-                            tablePrinter[i][4] = (function (v1, v2) {
-                                var str = '';
-                                var result = compareVersion( v1, v2 );
-                                if( result === 1 || result === 0) {
-                                    str = '不处理';
-                                    allFileArray[i][4] = 'noUpdate';
-                                }else if( result === -1 ) {
-                                    hasUpdatePackageModule = true; 
-                                    allFileArray[i][4] = 'update';
-                                    allUpdate ++;
-                                    str = '升级';
-                                }
-                                return str;
-                            })( allFileArray[i][2].substring(1), allFileArray[i][3] );
-                        });
-                        return ''
-                    });
-            })
-            .then( d2 => {
-                console.log(tablePrinter.toString());
-                // shellUpdate();
-                readlineHandler(shellUpdate);
-            })
-            .catch( err => {
-                console.log('out err', err);
+        // 安装
+        if ( mode && dirdata.length ) {
+            hasUpdatePackageModule = true;
+            dirdata.map( ( o, i ) => {
+                allFileArray[i] = [ i, o, updatePackgeName ];
+                tablePrinter.push( [ i, o, updatePackgeName ] );
+                allUpdate ++;
             });
-       
+            console.log(tablePrinter.toString());
+            readlineHandler(shellUpdate);
+        }else {   //更新
+            var allpipie = [];
+            var itempipe = [];
+            dirdata.map( (o, i) => {
+                var itemPromise = new Promise((resolve, reject) => {
+                    handlerPackage( o )
+                        .then(packdata => {
+                            if( packdata !== 'null' ) {
+                                resolve( o );
+                            }else {
+                                resolve( false );
+                            }
+                        })
+                })
+                itempipe.push(itemPromise);
+            });
+            
+            Promise.all(itempipe)
+                .then(data => {
+                data.filter( o => o !== false).map((_ , i) => {
+                        allFileArray[i] = [ i, _, '', updateVersion ];
+                        tablePrinter.push( [i , _, '', updateVersion ] );
+                        allpipie.push(handlerPackage( _ ));
+                });
+                return data;
+                })
+                .then(itemData => {
+                    return Promise.all(allpipie)
+                        .then(packdataArray => {
+                            packdataArray.map((o, i) => {
+                                allFileArray[i][2] = o;
+                                tablePrinter[i][2] = o;
+                                tablePrinter[i][4] = (function (v1, v2) {
+                                    var str = '';
+                                    var result = compareVersion( v1, v2 );
+                                    if( result === 1 || result === 0) {
+                                        str = '不处理';
+                                        allFileArray[i][4] = 'noUpdate';
+                                    }else if( result === -1 ) {
+                                        hasUpdatePackageModule = true; 
+                                        allFileArray[i][4] = 'update';
+                                        allUpdate ++;
+                                        str = '升级';
+                                    }
+                                    return str;
+                                })( allFileArray[i][2].substring(1), allFileArray[i][3] );
+                            });
+                            return '';
+                        });
+                })
+                .then( d2 => {
+                    console.log(tablePrinter.toString());
+                    // shellUpdate();
+                    readlineHandler(shellUpdate);
+                })
+                .catch( err => {
+                    console.log('out err', err);
+                });
+        }     
     }).catch( err => {
         std(1, err);
     });
@@ -129,23 +146,24 @@ function shellUpdate () {
         total: allUpdate + 1   //总共上传的数量
     });
     bar.tick();
-
     for( var i in allFileArray ) {
         var item = allFileArray[i];
         var itemPath = path.join( rootPath, item[1] );
-        var isUp = item[4] === 'update';
-        if(!isUp) {
-            continue;
-            return;
+        if ( commanderType === 'update' ) {
+            var isUp = item[4] === 'update';
+            if(!isUp) {
+                continue;
+                return;
+            }
         }
         //shell
         cd(itemPath);
-        var loadingUp = exec(`git checkout develop && git pull && npm install ${updatePackgeName}@${updateVersion} --save && git add . && git commit -m 'update version ${updatePackgeName} to ${updateVersion} by Automatic script' && git push && git checkout daily && git pull && git merge develop && git push && git checkout test && git pull && git merge daily && git push`, { encoding: 'buffer', async: false });
+        var loadingUp = exec( runModeEnv, { encoding: 'buffer', async: false });
         if( loadingUp.code === 0 ) {
             allFineshed ++;
             bar.tick(allFineshed);
             if( bar.complete ) {
-                allFineshedHandler()
+                allFineshedHandler();
             }
         }else {
             std(1, '你写的有问题啊');
@@ -235,6 +253,42 @@ function compareVersion (v1, v2) {
     }
 }
 
+//处理接受参数
+function handlerStdIn ( argv, mode ) {
+    rootPath = argv.c;
+    updatePackgeName = argv.p;
+    updateVersion = argv.v;
+    //安装
+    if ( mode ) {
+        tablePrinter = new Table({
+            head: ['索引', '匹配到的项目', '安装的包名'],
+            colWidths: [8, 30, 35]
+        });
+        runEnvCollect = {
+            'most': `git checkout develop && git pull && npm install ${updatePackgeName} --save && git add . && git commit -m 'install ${updatePackgeName} by Automatic script' && git push && git checkout daily && git pull && git merge develop && git push && git checkout test && git pull && git merge daily && git push`,
+            'develop': `git checkout develop && git pull && npm install ${updatePackgeName} --save && git add . && git commit -m 'install ${updatePackgeName} by Automatic script' && git push `,
+            'daily': `git checkout daily && git pull && npm install ${updatePackgeName} --save && git add . && git commit -m 'install ${updatePackgeName} by Automatic script' && git push `,
+            'test': `git checkout test && git pull && npm install ${updatePackgeName} --save && git add . && git commit -m 'install ${updatePackgeName} by Automatic script' && git push `
+        };
+    } else { //更新
+        tablePrinter = new Table({
+            head: ['索引', '匹配到的项目', `当前${updatePackgeName}的版本号`, `${updatePackgeName}升级到的版本号`, '处理'],
+            colWidths: [8, 30, 25, 25, 15]
+        });
+        runEnvCollect = {
+            'most': `git checkout develop && git pull && npm install ${updatePackgeName}@${updateVersion} --save && git add . && git commit -m 'update version ${updatePackgeName} to ${updateVersion} by Automatic script' && git push && git checkout daily && git pull && git merge develop && git push && git checkout test && git pull && git merge daily && git push`,
+            'develop': `git checkout develop && git pull && npm install ${updatePackgeName}@${updateVersion} --save && git add . && git commit -m 'update version ${updatePackgeName} to ${updateVersion} by Automatic script' && git push `,
+            'daily': `git checkout daily && git pull && npm install ${updatePackgeName}@${updateVersion} --save && git add . && git commit -m 'update version ${updatePackgeName} to ${updateVersion} by Automatic script' && git push `,
+            'test': `git checkout test && git pull && npm install ${updatePackgeName}@${updateVersion} --save && git add . && git commit -m 'update version ${updatePackgeName} to ${updateVersion} by Automatic script' && git push `
+        };
+    }
+    if ( argv.e ) {
+        runModeEnv = runEnvCollect[argv.e];
+    }else {
+        runModeEnv = runEnvCollect.most;
+    };
+}
+
 const yargs = require('yargs')
                 .command(
                     'update',                       //命令CMD
@@ -271,19 +325,62 @@ const yargs = require('yargs')
                                  description: '需要升级到的版本号',
                                  demand: true,
                                  type: 'string'
+                             },
+                             'e': {
+                                 description: '哪个环境发布，不写就是develop,daily,test同时发布',
+                                 demand: false
                              }
                         });
                     },
                     argv => {                   //handler  接受命令行参数
-                        rootPath = argv.c;
-                        updatePackgeName = argv.p;
-                        updateVersion = argv.v;
-                        tablePrinter = new Table({
-                            head: ['索引', '匹配到的项目', `当前${updatePackgeName}的版本号`, `${updatePackgeName}升级到的版本号`, '处理'],
-                            colWidths: [8, 30, 25, 25, 15]
-                        });
-
+                        handlerStdIn( argv );   //处理参数
                         handlerAllFile( argv.c, argv.k, argv.s );
+                    }
+                )
+                .command(  //安装
+                    'install',
+                    'install some npm package',
+                    yargs => {
+                        return yargs.option({
+                            'k': {
+                                alias: 'keyword',
+                                description: '输入关键字来匹配项目',
+                                // default: 'phoenix', //该参数的默认值
+                                type: 'string'   //参数类型
+                             },
+                             's': {   //多参数形式的 特别指定项目名  和上面的  k  二选一哦
+                                alias: 'special',
+                                description: '多参数选择项目名',
+                                array: 'special'
+                             },
+                             'p': {
+                                alias: 'packageName',
+                                description: '输入包名',
+                                demand: true,  //必选参数
+                                default: 'tm-c-oss', //该参数的默认值
+                                type: 'string'   //参数类型
+                             },
+                             'c': {
+                                 alias: 'cwd',
+                                 description: '包含所有项目的目录绝对路径',
+                                 demand: true,
+                                 default: '/Users/qsm/Code',
+                                 type: 'string' 
+                             },
+                             'v': {
+                                 description: '需要安装到的版本号',
+                                 type: 'string'
+                             },
+                            'e': {
+                                 description: '哪个环境发布，不写就是develop,daily,test同时发布',
+                                 demand: false
+                            }
+                        })
+                    },
+                    argv => {
+                        commanderType = 'install'
+                        handlerStdIn( argv, 'install' );
+                        handlerAllFile( argv.c, argv.k, argv.s, 'install');
                     }
                 )
                 .help('h')   //  输入 -h 显示帮助信息
